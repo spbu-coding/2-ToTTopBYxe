8067b0b340bbc6d295898dc0772959c0f5cbd687
ToTTopBYxe <andrey22000022@gmail.com>
2020-10-31 22:14:30 +0600

    homework

--- src/main.c
+++ src/main.c
@@ -2,45 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern void bubble_sort(void);
+void bubble_sort(int *mass, int *mass_without_sorting, int demension, int counter);
 
 // Получаем параметр To
-int getTo(char* string, int previous_result){
+int getTo(char *string, int previous_result) {
     int result = previous_result;
 
     if (strstr(string, "--to=") != NULL) {
         if (previous_result != -1) result = -2; // Параметр встретился второй раз
-        else result = atoi(string+5); // распознаем цифры при смещении указателя на 5 (длина "--to=")
+        else result = atoi(string + 5); // распознаем цифры при смещении указателя на 5 (длина "--to=")
     }
 
     return result;
 }
 
 // Получаем параметр From
-int getFrom(char* string, int previous_result){
+int getFrom(char *string, int previous_result) {
     int result = previous_result;
 
     if (strstr(string, "--from=") != NULL) {
         if (previous_result != -1) result = -2; // Параметр встретился второй раз
-        else result = atoi(string+7); // распознаем цифры при смещении указателся на 7 (длина "--from=")
+        else result = atoi(string + 7); // распознаем цифры при смещении указателся на 7 (длина "--from=")
     }
 
     return result;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     int main_array[100]; // 100 элементов
     int from = -1; // -1 - переменная не определена
     int to = -1;
     int main_result = 0; // количество элементов, участвовавших в сортировке и изменивших свою позицию
-    int wrong_nums_size=0, cleared_mass_size=0;//размер массиива с неподходящими числами и размер очищенного массива
-    int* cleared_mass[100], cleared_mass_wihout_sorting[100], wrong_nums[100]; //очищенный от невходящих в границы чисел массив и массив с неподходящими числами невошедшими в сортировку
-    int array_size=0;
-    int j=0, k=0; //счетчики для записи в цикле элементов  в массивы cleared_mass и wrong_nums
+    int wrong_nums_size = 0, cleared_mass_size = 0;//размер массиива с неподходящими числами и размер очищенного массива
+    int cleared_mass[100], cleared_mass_wihout_sorting[100], wrong_nums[100]; //очищенный от невходящих в границы чисел массив и массив с неподходящими числами невошедшими в сортировку
+    int array_size = 0;
+    int j = 0, k = 0; //счетчики для записи в цикле элементов  в массивы cleared_mass и wrong_nums
 
     if (argc < 2) return -1; // введено менее 1 параметра
-    if (argc > 3) {return -2; exit(-2);} // введено более 2-х параметров
+    if (argc > 3) {
+        return -2;
+    } // введено более 2-х параметров
 
 
     for (int i = 0; i < argc; ++i) {
@@ -58,98 +59,76 @@ int main(int argc, char** argv)
         array_size++;
     }
 
-    for(int i = 0; i < array_size; i ++)
-        if(from!=-1 && from!=-2)
-        {
-            if(to!=-1 && to!=-2)
-            {
-             if(main_array[i] <= from || main_array[i] >= to)
-             {
-              wrong_nums_size++;
-              wrong_nums[j] = main_array[i];
-              j++;
-             }
-             else
-             {
-              cleared_mass_size++;
-              cleared_mass[k] = &main_array[i];
-              cleared_mass_wihout_sorting[k] = main_array[i];
-              k++;
-             }
+    for (int i = 0; i < array_size; i++)
+        if (from != -1 && from != -2) {
+            if (to != -1 && to != -2) {
+                if (main_array[i] <= from || main_array[i] >= to) {
+                    wrong_nums_size++;
+                    wrong_nums[j] = main_array[i];
+                    j++;
+                } else {
+                    cleared_mass_size++;
+                    cleared_mass[k] = main_array[i];
+                    cleared_mass_wihout_sorting[k] = main_array[i];
+                    k++;
+                }
+            } else {
+                if (main_array[i] <= from) {
+                    wrong_nums_size++;
+                    wrong_nums[j] = main_array[i];
+                    j++;
+                } else {
+                    cleared_mass_size++;
+                    cleared_mass[k] = main_array[i];
+                    cleared_mass_wihout_sorting[k] = main_array[i];
+                    k++;
+                }
             }
-            else
-            {
-             if(main_array[i] <= from)
-             {
-              wrong_nums_size++;
-              wrong_nums[j] = main_array[i];
-              j++;
-             }
-             else
-             {
-              cleared_mass_size++;
-              cleared_mass[k] = &main_array[i];
-              cleared_mass_wihout_sorting[k] = main_array[i];
-              k++;
-             }
+        } else {
+            if (to != -1 && to != -2) {
+                if (main_array[i] >= to) {
+                    wrong_nums_size++;
+                    wrong_nums[j] = main_array[i];
+                    j++;
+                } else {
+                    cleared_mass_size++;
+                    cleared_mass[k] = main_array[i];
+                    cleared_mass_wihout_sorting[k] = main_array[i];
+                    k++;
+                }
+            } else {
+                cleared_mass_size++;
+                cleared_mass[k] = main_array[i];
+                cleared_mass_wihout_sorting[k] = main_array[i];
+                k++;
             }
         }
-        else {
-            if(to!=-1 && to!=-2)
-            {
-              if(main_array[i] >= to)
-             {
-              wrong_nums_size++;
-              wrong_nums[j] = main_array[i];
-              j++;
-             }
-             else
-             {
-              cleared_mass_size++;
-              cleared_mass[k] = &main_array[i];
-              cleared_mass_wihout_sorting[k] = main_array[i];
-              k++;
-             }
-            }
-            else
-            {
-              cleared_mass_size++;
-              cleared_mass[k] = &main_array[i];
-              cleared_mass_wihout_sorting[k] = main_array[i];
-              k++;
-            }
-            }
 
     printf("Params: ");
-    for(int i = 1; i < argc; i++)
-    {
+    for (int i = 1; i < argc; i++) {
         printf("%s ", argv[i]);
     }
     printf("\tInput: ");
-    for(int i = 0; i < array_size; i++)
-    {
+    for (int i = 0; i < array_size; i++) {
         printf("%d ", main_array[i]);
     }
     printf("\nStdout: ");
-    if(from >= 0)
+    if (from >= 0)
 
-    for(int i = 0; i < wrong_nums_size; i++)
-    {
-        if(wrong_nums[i] < from)
-            printf("%d ", wrong_nums[i]);
-    }
+        for (int i = 0; i < wrong_nums_size; i++) {
+            if (wrong_nums[i] < from)
+                printf("%d ", wrong_nums[i]);
+        }
     printf("\tStderr: ");
-    if(to >= 0)
-    for(int i = 0; i < wrong_nums_size; i++)
-    {
-        if(wrong_nums[i] > to)
-            printf("%d ", wrong_nums[i]);
-    }
+    if (to >= 0)
+        for (int i = 0; i < wrong_nums_size; i++) {
+            if (wrong_nums[i] > to)
+                printf("%d ", wrong_nums[i]);
+        }
 
     printf("\tReduced: ");
-    for(int i = 0; i < cleared_mass_size; i++)
-    {
-     printf("%d ", cleared_mass[i]);
+    for (int i = 0; i < cleared_mass_size; i++) {
+        printf("%d ", cleared_mass[i]);
     }
 
     // Sort Function in assembler
--- src/sort.s
+++ src/sort.s
@@ -2,17 +2,20 @@
 	.text
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
-	.string	"\tSorted: "
-.LC1:
 	.string	"%d "
+	.section	.rodata.str1.8,"aMS",@progbits,1
+	.align 8
+.LC1:
+	.string	"\nNumber of elements, involved in sorting and chaging position: %d"
+	.section	.rodata.str1.1
 .LC2:
-	.string	"\nProcess returned %d"
+	.string	"\tSorted: "
 	.text
 	.p2align 4,,15
 	.globl	bubble_sort
 	.type	bubble_sort, @function
 bubble_sort:
-.LFB41:
+.LFB23:
 	.cfi_startproc
 	pushq	%r13
 	.cfi_def_cfa_offset 16
@@ -20,76 +23,201 @@ bubble_sort:
 	pushq	%r12
 	.cfi_def_cfa_offset 24
 	.cfi_offset 12, -24
-	movl	%esi, %r12d
 	pushq	%rbp
 	.cfi_def_cfa_offset 32
 	.cfi_offset 6, -32
 	pushq	%rbx
 	.cfi_def_cfa_offset 40
 	.cfi_offset 3, -40
-	movq	%rdi, %rbp
-	movl	%edx, %ebx
+	movl	%ecx, %ebx
 	subq	$8, %rsp
 	.cfi_def_cfa_offset 48
-	testl	%esi, %esi
-	jle	.L2
-	leal	-2(%rsi), %r8d
-	leaq	4(%rdi), %r9
-	cmpl	$-1, %r8d
+	testl	%edx, %edx
+	jle	.L7
+	leal	-2(%rdx), %r10d
+	movq	%rdi, %r12
+	leaq	4(%rdi), %r11
+	cmpl	$-1, %r10d
 	je	.L2
 	.p2align 4,,10
 	.p2align 3
 .L6:
-	movl	%r8d, %edx
-	movq	%rbp, %rax
-	movl	$1, %edi
-	leaq	(%r9,%rdx,4), %rsi
+	movl	%r10d, %ecx
+	movq	%r12, %rax
+	movl	$1, %r9d
+	leaq	(%r11,%rcx,4), %rcx
 	.p2align 4,,10
 	.p2align 3
 .L5:
-	movl	(%rax), %edx
-	movl	4(%rax), %ecx
-	cmpl	%ecx, %edx
+	movl	(%rax), %edi
+	movl	4(%rax), %r8d
+	cmpl	%r8d, %edi
 	jle	.L4
-	movl	%ecx, (%rax)
-	movl	%edx, 4(%rax)
+	movl	%r8d, (%rax)
+	movl	%edi, 4(%rax)
 	addl	$1, %ebx
-	xorl	%edi, %edi
+	xorl	%r9d, %r9d
 .L4:
 	addq	$4, %rax
-	cmpq	%rsi, %rax
+	cmpq	%rcx, %rax
 	jne	.L5
-	testb	%dil, %dil
+	testb	%r9b, %r9b
 	jne	.L2
-	subl	$1, %r8d
-	cmpl	$-1, %r8d
+	subl	$1, %r10d
+	cmpl	$-1, %r10d
 	jne	.L6
 .L2:
-	leaq	.LC0(%rip), %rsi
-	xorl	%eax, %eax
-	movl	$1, %edi
-	call	__printf_chk@PLT
-	testl	%r12d, %r12d
+	testl	%edx, %edx
 	jle	.L7
-	leal	-1(%r12), %eax
-	leaq	.LC1(%rip), %r12
-	leaq	4(%rbp,%rax,4), %r13
+	movq	%r12, %rax
+	leal	-1(%rdx), %r13d
+	movl	$5, %edi
+	shrq	$2, %rax
+	movq	%r12, %rbp
+	negq	%rax
+	andl	$3, %eax
+	leal	3(%rax), %ecx
+	cmpl	$5, %ecx
+	cmovb	%edi, %ecx
+	cmpl	%ecx, %r13d
+	jb	.L26
+	xorl	%r9d, %r9d
+	testl	%eax, %eax
+	je	.L9
+	movl	(%rsi), %ecx
+	cmpl	%ecx, (%r12)
+	movl	$1, %r9d
+	setne	%cl
+	movzbl	%cl, %ecx
+	addl	%ecx, %ebx
+	cmpl	$1, %eax
+	je	.L9
+	movl	4(%rsi), %ecx
+	cmpl	%ecx, 4(%r12)
+	movl	$2, %r9d
+	setne	%cl
+	movzbl	%cl, %ecx
+	addl	%ecx, %ebx
+	cmpl	$3, %eax
+	jne	.L9
+	movl	8(%r12), %ecx
+	cmpl	%ecx, 8(%rsi)
+	movl	$3, %r9d
+	setne	%cl
+	movzbl	%cl, %ecx
+	addl	%ecx, %ebx
+.L9:
+	movl	%edx, %r8d
+	movl	%eax, %ecx
+	xorl	%edi, %edi
+	subl	%eax, %r8d
+	pxor	%xmm1, %xmm1
+	pcmpeqd	%xmm2, %xmm2
+	salq	$2, %rcx
+	movl	%r8d, %r10d
+	leaq	(%r12,%rcx), %r11
+	xorl	%eax, %eax
+	shrl	$2, %r10d
+	addq	%rsi, %rcx
 	.p2align 4,,10
 	.p2align 3
+.L15:
+	movdqu	(%rcx,%rax), %xmm0
+	addl	$1, %edi
+	pcmpeqd	(%r11,%rax), %xmm0
+	addq	$16, %rax
+	cmpl	%edi, %r10d
+	pandn	%xmm2, %xmm0
+	psubd	%xmm0, %xmm1
+	ja	.L15
+	movdqa	%xmm1, %xmm0
+	movl	%r8d, %ecx
+	andl	$-4, %ecx
+	psrldq	$8, %xmm0
+	paddd	%xmm0, %xmm1
+	movdqa	%xmm1, %xmm0
+	psrldq	$4, %xmm0
+	paddd	%xmm0, %xmm1
+	movd	%xmm1, %eax
+	addl	%eax, %ebx
+	cmpl	%ecx, %r8d
+	leal	(%rcx,%r9), %eax
+	je	.L16
 .L8:
+	movslq	%eax, %rcx
+	movl	(%rsi,%rcx,4), %edi
+	cmpl	%edi, (%r12,%rcx,4)
+	je	.L17
+	addl	$1, %ebx
+.L17:
+	leal	1(%rax), %ecx
+	cmpl	%edx, %ecx
+	jge	.L16
+	movslq	%ecx, %rcx
+	movl	(%r12,%rcx,4), %edi
+	cmpl	%edi, (%rsi,%rcx,4)
+	je	.L18
+	addl	$1, %ebx
+.L18:
+	leal	2(%rax), %ecx
+	cmpl	%edx, %ecx
+	jge	.L16
+	movslq	%ecx, %rcx
+	movl	(%rsi,%rcx,4), %edi
+	cmpl	%edi, (%r12,%rcx,4)
+	je	.L19
+	addl	$1, %ebx
+.L19:
+	leal	3(%rax), %ecx
+	cmpl	%ecx, %edx
+	jle	.L16
+	movslq	%ecx, %rcx
+	movl	(%rsi,%rcx,4), %edi
+	cmpl	%edi, (%r12,%rcx,4)
+	je	.L20
+	addl	$1, %ebx
+.L20:
+	leal	4(%rax), %ecx
+	cmpl	%ecx, %edx
+	jle	.L16
+	movslq	%ecx, %rcx
+	movl	(%rsi,%rcx,4), %edi
+	cmpl	%edi, (%r12,%rcx,4)
+	je	.L21
+	addl	$1, %ebx
+.L21:
+	addl	$5, %eax
+	cmpl	%eax, %edx
+	jle	.L16
+	cltq
+	movl	(%rsi,%rax,4), %esi
+	cmpl	%esi, (%r12,%rax,4)
+	je	.L16
+	addl	$1, %ebx
+.L16:
+	leaq	.LC2(%rip), %rsi
+	leaq	4(%r12,%r13,4), %r13
+	leaq	.LC0(%rip), %r12
+	movl	$1, %edi
+	xorl	%eax, %eax
+	call	__printf_chk@PLT
+	.p2align 4,,10
+	.p2align 3
+.L24:
 	movl	0(%rbp), %edx
 	xorl	%eax, %eax
 	movq	%r12, %rsi
 	movl	$1, %edi
 	addq	$4, %rbp
 	call	__printf_chk@PLT
-	cmpq	%rbp, %r13
-	jne	.L8
-.L7:
+	cmpq	%r13, %rbp
+	jne	.L24
+.L25:
 	addq	$8, %rsp
+	.cfi_remember_state
 	.cfi_def_cfa_offset 40
 	movl	%ebx, %edx
-	leaq	.LC2(%rip), %rsi
+	leaq	.LC1(%rip), %rsi
 	popq	%rbx
 	.cfi_def_cfa_offset 32
 	popq	%rbp
@@ -101,8 +229,18 @@ bubble_sort:
 	movl	$1, %edi
 	xorl	%eax, %eax
 	jmp	__printf_chk@PLT
+.L7:
+	.cfi_restore_state
+	leaq	.LC2(%rip), %rsi
+	movl	$1, %edi
+	xorl	%eax, %eax
+	call	__printf_chk@PLT
+	jmp	.L25
+.L26:
+	xorl	%eax, %eax
+	jmp	.L8
 	.cfi_endproc
-.LFE41:
+.LFE23:
 	.size	bubble_sort, .-bubble_sort
 	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
 	.section	.note.GNU-stack,"",@progbits
