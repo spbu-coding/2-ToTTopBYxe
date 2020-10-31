a24fb12eee4a2f6c550167e0b2eb3fbaa836f04e
ToTTopBYxe <andrey22000022@gmail.com>
2020-10-31 21:34:51 +0600

    homework

--- src/sort.s
+++ src/sort.s
@@ -1,107 +1,108 @@
 	.file	"sort.c"
 	.text
-	.section .rdata,"dr"
+	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
-	.ascii "%d \0"
-	.align 8
+	.string	"\tSorted: "
 .LC1:
-	.ascii "Number of elements, involved in sorting  and changing position: %d\0"
+	.string	"%d "
 .LC2:
-	.ascii "\11Sorted: \0"
+	.string	"\nProcess returned %d"
 	.text
 	.p2align 4,,15
 	.globl	bubble_sort
-	.def	bubble_sort;	.scl	2;	.type	32;	.endef
-	.seh_proc	bubble_sort
+	.type	bubble_sort, @function
 bubble_sort:
+.LFB41:
+	.cfi_startproc
+	pushq	%r13
+	.cfi_def_cfa_offset 16
+	.cfi_offset 13, -16
+	pushq	%r12
+	.cfi_def_cfa_offset 24
+	.cfi_offset 12, -24
+	movl	%esi, %r12d
 	pushq	%rbp
-	.seh_pushreg	%rbp
-	pushq	%rdi
-	.seh_pushreg	%rdi
-	pushq	%rsi
-	.seh_pushreg	%rsi
+	.cfi_def_cfa_offset 32
+	.cfi_offset 6, -32
 	pushq	%rbx
-	.seh_pushreg	%rbx
-	subq	$40, %rsp
-	.seh_stackalloc	40
-	.seh_endprologue
-	movq	%rcx, %rbx
-	movl	%r9d, %esi
-	testl	%r8d, %r8d
-	jle	.L15
-	leal	-2(%r8), %edi
-	leaq	4(%rcx), %rbp
-	.p2align 4,,10
-.L3:
-	cmpl	$-1, %edi
+	.cfi_def_cfa_offset 40
+	.cfi_offset 3, -40
+	movq	%rdi, %rbp
+	movl	%edx, %ebx
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 48
+	testl	%esi, %esi
+	jle	.L2
+	leal	-2(%rsi), %r8d
+	leaq	4(%rdi), %r9
+	cmpl	$-1, %r8d
 	je	.L2
-	movl	%edi, %ecx
-	movq	%rbx, %rax
-	movl	$1, %r11d
-	leaq	0(%rbp,%rcx,4), %r10
 	.p2align 4,,10
-.L7:
-	movl	(%rax), %ecx
-	movl	4(%rax), %r9d
-	cmpl	%r9d, %ecx
-	jle	.L6
-	movl	%r9d, (%rax)
-	xorl	%r11d, %r11d
-	movl	%ecx, 4(%rax)
+	.p2align 3
 .L6:
-	addq	$4, %rax
-	cmpq	%r10, %rax
-	jne	.L7
-	subl	$1, %edi
-	testb	%r11b, %r11b
-	je	.L3
+	movl	%r8d, %edx
+	movq	%rbp, %rax
+	movl	$1, %edi
+	leaq	(%r9,%rdx,4), %rsi
 	.p2align 4,,10
-.L2:
-	leal	-1(%r8), %ecx
+	.p2align 3
+.L5:
+	movl	(%rax), %edx
+	movl	4(%rax), %ecx
+	cmpl	%ecx, %edx
+	jle	.L4
+	movl	%ecx, (%rax)
+	movl	%edx, 4(%rax)
+	addl	$1, %ebx
 	xorl	%edi, %edi
-	testl	%r8d, %r8d
-	jg	.L10
-	jmp	.L15
+.L4:
+	addq	$4, %rax
+	cmpq	%rsi, %rax
+	jne	.L5
+	testb	%dil, %dil
+	jne	.L2
+	subl	$1, %r8d
+	cmpl	$-1, %r8d
+	jne	.L6
+.L2:
+	leaq	.LC0(%rip), %rsi
+	xorl	%eax, %eax
+	movl	$1, %edi
+	call	__printf_chk@PLT
+	testl	%r12d, %r12d
+	jle	.L7
+	leal	-1(%r12), %eax
+	leaq	.LC1(%rip), %r12
+	leaq	4(%rbp,%rax,4), %r13
 	.p2align 4,,10
-.L14:
-	movq	%rax, %rdi
-.L10:
-	movl	(%rdx,%rdi,4), %eax
-	cmpl	%eax, (%rbx,%rdi,4)
-	je	.L9
-	addl	$1, %esi
-.L9:
-	leaq	1(%rdi), %rax
-	cmpq	%rcx, %rdi
-	jne	.L14
-	leaq	.LC2(%rip), %rcx
-	leaq	4(%rbx,%rdi,4), %rbp
-	call	printf
-	leaq	.LC0(%rip), %rdi
-	.p2align 4,,10
-.L12:
-	movl	(%rbx), %edx
-	movq	%rdi, %rcx
-	addq	$4, %rbx
-	call	printf
-	cmpq	%rbx, %rbp
-	jne	.L12
-.L13:
-	movl	$10, %ecx
-	call	putchar
-	movl	%esi, %edx
-	leaq	.LC1(%rip), %rcx
-	addq	$40, %rsp
+	.p2align 3
+.L8:
+	movl	0(%rbp), %edx
+	xorl	%eax, %eax
+	movq	%r12, %rsi
+	movl	$1, %edi
+	addq	$4, %rbp
+	call	__printf_chk@PLT
+	cmpq	%rbp, %r13
+	jne	.L8
+.L7:
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 40
+	movl	%ebx, %edx
+	leaq	.LC2(%rip), %rsi
 	popq	%rbx
-	popq	%rsi
-	popq	%rdi
+	.cfi_def_cfa_offset 32
 	popq	%rbp
-	jmp	printf
-.L15:
-	leaq	.LC2(%rip), %rcx
-	call	printf
-	jmp	.L13
-	.seh_endproc
-	.ident	"GCC: (GNU) 8.3.0"
-	.def	printf;	.scl	2;	.type	32;	.endef
-	.def	putchar;	.scl	2;	.type	32;	.endef
+	.cfi_def_cfa_offset 24
+	popq	%r12
+	.cfi_def_cfa_offset 16
+	popq	%r13
+	.cfi_def_cfa_offset 8
+	movl	$1, %edi
+	xorl	%eax, %eax
+	jmp	__printf_chk@PLT
+	.cfi_endproc
+.LFE41:
+	.size	bubble_sort, .-bubble_sort
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
